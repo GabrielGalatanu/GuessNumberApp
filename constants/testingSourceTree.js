71b8f699b4277228ac473d92b928a3af71035162
Gabi Galatanu <mihai.gabriel.galatanu@gmail.com>
2021-09-03 08:26:59 +0300

    Test commit SourceTree.

---
+++ constants/testingSourceTree.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Text} from 'react-native';
+
+const testSourceTree = () => {
+
+    return (
+        <Text> Hello </Text>
+    )
+}
+
+export default testSourceTree;
