f04d2769d67924aa59613671a1222064e57e38ad
Gabi Galatanu <mihai.gabriel.galatanu@gmail.com>
2021-09-03 08:06:55 +0300

    1. commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
+
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
+
+export default function App() {
+
+
+
+
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a number" />
+      <StartGameScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  }
+});
